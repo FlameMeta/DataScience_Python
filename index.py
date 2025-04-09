98864a2d0b49058ab78886040d40dc964eeb0dd2
RidwanRosling <ridwansbdt@gmail.com>
2025-04-09 16:55:48 +0700

    simple example of using matplotlib in python

---
+++ index.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+# Data
+x = [1, 2, 3, 4, 5]
+y = [2, 3, 5, 7, 11]
+
+# Membuat plot
+plt.plot(x, y)
+
+# Memberi label
+plt.title("Contoh Grafik Garis")
+plt.xlabel("Nilai x")
+plt.ylabel("Nilai y")
+
+# Menampilkan grafik
+plt.show()
