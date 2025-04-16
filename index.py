672fc74f00756b29644d48e13be9e3922d736def
RidwanRosling <ridwansbdt@gmail.com>
2025-04-16 11:22:18 +0700

    add data csv

--- index.py
+++ index.py
@@ -1,16 +1,28 @@
+import pandas as pd
 import matplotlib.pyplot as plt
+data = pd.read_csv('data_csv/data1.csv')
 
 # Data
-x = [1, 2, 3, 4, 5]
-y = [2, 3, 5, 7, 11]
+# x = [1, 2, 3, 4, 5]
+# y = [2, 3, 5, 7, 11]
+# a = [1, 2, 3, 4, 5]
+# b = [2, 3, 5, 7, 11]
 
-# Membuat plot
-plt.plot(x, y)
+# # Membuat plot
+# plt.plot(x, y)
+# plt.plot(a, b)
 
-# Memberi label
-plt.title("Contoh Grafik Garis")
-plt.xlabel("Nilai x")
-plt.ylabel("Nilai y")
+# # Memberi label
+# plt.title("Contoh Grafik Garis")
+# plt.xlabel("Nilai x")
+# plt.ylabel("Nilai y")
 
-# Menampilkan grafik
+# # Menampilkan grafik
+# plt.show()
+
+plt.plot(data['kota'], data['percentase'])
+plt.title('Grafik Penjualan')
+plt.xlabel('Tanggal')
+plt.ylabel('Penjualan')
+plt.grid(True)
 plt.show()
