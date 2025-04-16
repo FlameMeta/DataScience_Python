d7578905f73c4bc2f41b08dbda0a2b3d5de9b8e3
kamiyamafitra <aprianidewi293@gmail.com>
2025-04-16 11:58:51 +0700

    add tail

--- index.py
+++ index.py
@@ -2,10 +2,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 data1 = pd.read_csv('data_csv/data1.csv')
-data2= pd.read_csv('data_csv/data2.csv')
+# data2= pd.read_csv('data_csv/data2.csv')
+data1.index = range(1, len(data1) + 1)
 
 
-gabungan = pd.concat([data1, data2], ignore_index=True)
+# gabungan = pd.concat([data1, data2], ignore_index=True)
 
 # Tampilkan 5 baris pertama
-print(gabungan.head())
+print(data1.head())
+print(data1.tail())
+# data2.tail()
