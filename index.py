a8df286b636cec5c752f1947da6b607ee806acfa
kamiyamafitra <aprianidewi293@gmail.com>
2025-04-16 12:39:31 +0700

    minor changes

--- index.py
+++ index.py
@@ -1,14 +1,21 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from datetime import date
 
-data1 = pd.read_csv('data_csv/data1.csv')
+data1 = pd.read_csv('data_csv/data1_dengan_tanggal.csv')
 # data2= pd.read_csv('data_csv/data2.csv')
 data1.index = range(1, len(data1) + 1)
 
 
-# gabungan = pd.concat([data1, data2], ignore_index=True)
 
 # Tampilkan 5 baris pertama
+print(data1.dtypes)
+
+
 print(data1.head())
 print(data1.tail())
-# data2.tail()
+#dtype
+print(data1.info())
+#obejct
+print(data1.nunique())
+
