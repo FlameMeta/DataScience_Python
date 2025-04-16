8ad0fcd1e96da862018743bb0631608f01e5c7c8
RidwanRosling <ridwansbdt@gmail.com>
2025-04-16 11:44:57 +0700

    add csv

--- index.py
+++ index.py
@@ -1,28 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-data = pd.read_csv('data_csv/data1.csv')
 
-# Data
-# x = [1, 2, 3, 4, 5]
-# y = [2, 3, 5, 7, 11]
-# a = [1, 2, 3, 4, 5]
-# b = [2, 3, 5, 7, 11]
+data1 = pd.read_csv('data_csv/data1.csv')
+data2= pd.read_csv('data_csv/data2.csv')
 
-# # Membuat plot
-# plt.plot(x, y)
-# plt.plot(a, b)
 
-# # Memberi label
-# plt.title("Contoh Grafik Garis")
-# plt.xlabel("Nilai x")
-# plt.ylabel("Nilai y")
+gabungan = pd.concat([data1, data2], ignore_index=True)
 
-# # Menampilkan grafik
-# plt.show()
-
-plt.plot(data['kota'], data['percentase'])
-plt.title('Grafik Penjualan')
-plt.xlabel('Tanggal')
-plt.ylabel('Penjualan')
-plt.grid(True)
-plt.show()
+# Tampilkan 5 baris pertama
+print(gabungan.head())
