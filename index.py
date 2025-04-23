c5560442cc39fa92cbc11e6fbe7808762cb3ae7e
User-CruZer <ahmadzakialbadri@gmail.com>
2025-04-23 11:24:49 +0700

    added my own working space named after me, zaki himself

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import date
 
-data1 = pd.read_csv('data_csv/data1_dengan_tanggal.csv')
+data1 = pd.read_csv('data_csv/data1.csv')
 # data2= pd.read_csv('data_csv/data2.csv')
 data1.index = range(1, len(data1) + 1)
 
